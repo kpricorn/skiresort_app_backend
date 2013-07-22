aa5cf716b9d21fe40e18c4ef845bd1ba2835eb52
Sebastian de Castelberg <sebu@kpricorn.org>
2013-07-22 11:05:47 +0200

    basic config

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Fai4mieyohshi4ahnaethahF8oY7aothulaif3va');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', 'ediem0em6Cha3iesovooP6ieg6EiLiequ9iyeijo');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
@@ -239,7 +239,7 @@
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+  date_default_timezone_set('UTC');
 
 /**
  *
