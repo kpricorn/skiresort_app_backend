1220420e7342039ab47efb6bf62df971164e9bf8
Sebastian de Castelberg <sebu@kpricorn.org>
2013-07-22 12:33:56 +0200

    implement contests model/view/controller
    admin section wich crud views
    
    contests.xml -> renders xml view

---
+++ app/Model/Contest.php
@@ -0,0 +1,33 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Contest Model
+ *
+ */
+class Contest extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+  public $displayField = 'maintext';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+  public $validate = array(
+    'maintext' => array(
+      'notempty' => array(
+        'rule' => array('notempty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    )
+  );
+}
