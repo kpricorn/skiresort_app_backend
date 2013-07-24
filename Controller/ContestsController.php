a0f63b61e5e2da4f2581ad983eba590ab005f000
Sebastian de Castelberg <sebu@kpricorn.org>
2013-07-24 14:50:51 +0200

    cleanup whitespaces

--- Controller/ContestsController.php
+++ Controller/ContestsController.php
@@ -12,10 +12,10 @@ class ContestsController extends AppController {
  *
  * @return void
  */
-	public function index() {
-		$this->Contest->recursive = 0;
-		$this->set('contests', $this->paginate());
-	}
+  public function index() {
+    $this->Contest->recursive = 0;
+    $this->set('contests', $this->paginate());
+  }
 
 /**
  * view method
@@ -24,30 +24,30 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function view($id = null) {
-		if (!$this->Contest->exists($id)) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		$options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
-		$this->set('contest', $this->Contest->find('first', $options));
-	}
+  public function view($id = null) {
+    if (!$this->Contest->exists($id)) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    $options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
+    $this->set('contest', $this->Contest->find('first', $options));
+  }
 
 /**
  * add method
  *
  * @return void
  */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Contest->create();
-			if ($this->Contest->save($this->request->data)) {
-				$this->Session->setFlash(__('The contest has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
-			}
-		}
-	}
+  public function add() {
+    if ($this->request->is('post')) {
+      $this->Contest->create();
+      if ($this->Contest->save($this->request->data)) {
+        $this->Session->setFlash(__('The contest has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
+      }
+    }
+  }
 
 /**
  * edit method
@@ -56,22 +56,22 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function edit($id = null) {
-		if (!$this->Contest->exists($id)) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Contest->save($this->request->data)) {
-				$this->Session->setFlash(__('The contest has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
-			$this->request->data = $this->Contest->find('first', $options);
-		}
-	}
+  public function edit($id = null) {
+    if (!$this->Contest->exists($id)) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    if ($this->request->is('post') || $this->request->is('put')) {
+      if ($this->Contest->save($this->request->data)) {
+        $this->Session->setFlash(__('The contest has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
+      }
+    } else {
+      $options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
+      $this->request->data = $this->Contest->find('first', $options);
+    }
+  }
 
 /**
  * delete method
@@ -80,29 +80,29 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function delete($id = null) {
-		$this->Contest->id = $id;
-		if (!$this->Contest->exists()) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		$this->request->onlyAllow('post', 'delete');
-		if ($this->Contest->delete()) {
-			$this->Session->setFlash(__('Contest deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Contest was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+  public function delete($id = null) {
+    $this->Contest->id = $id;
+    if (!$this->Contest->exists()) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    $this->request->onlyAllow('post', 'delete');
+    if ($this->Contest->delete()) {
+      $this->Session->setFlash(__('Contest deleted'));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->Session->setFlash(__('Contest was not deleted'));
+    $this->redirect(array('action' => 'index'));
+  }
 
 /**
  * admin_index method
  *
  * @return void
  */
-	public function admin_index() {
-		$this->Contest->recursive = 0;
-		$this->set('contests', $this->paginate());
-	}
+  public function admin_index() {
+    $this->Contest->recursive = 0;
+    $this->set('contests', $this->paginate());
+  }
 
 /**
  * admin_view method
@@ -111,30 +111,30 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function admin_view($id = null) {
-		if (!$this->Contest->exists($id)) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		$options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
-		$this->set('contest', $this->Contest->find('first', $options));
-	}
+  public function admin_view($id = null) {
+    if (!$this->Contest->exists($id)) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    $options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
+    $this->set('contest', $this->Contest->find('first', $options));
+  }
 
 /**
  * admin_add method
  *
  * @return void
  */
-	public function admin_add() {
-		if ($this->request->is('post')) {
-			$this->Contest->create();
-			if ($this->Contest->save($this->request->data)) {
-				$this->Session->setFlash(__('The contest has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
-			}
-		}
-	}
+  public function admin_add() {
+    if ($this->request->is('post')) {
+      $this->Contest->create();
+      if ($this->Contest->save($this->request->data)) {
+        $this->Session->setFlash(__('The contest has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
+      }
+    }
+  }
 
 /**
  * admin_edit method
@@ -143,22 +143,22 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function admin_edit($id = null) {
-		if (!$this->Contest->exists($id)) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Contest->save($this->request->data)) {
-				$this->Session->setFlash(__('The contest has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
-			$this->request->data = $this->Contest->find('first', $options);
-		}
-	}
+  public function admin_edit($id = null) {
+    if (!$this->Contest->exists($id)) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    if ($this->request->is('post') || $this->request->is('put')) {
+      if ($this->Contest->save($this->request->data)) {
+        $this->Session->setFlash(__('The contest has been saved'));
+        $this->redirect(array('action' => 'index'));
+      } else {
+        $this->Session->setFlash(__('The contest could not be saved. Please, try again.'));
+      }
+    } else {
+      $options = array('conditions' => array('Contest.' . $this->Contest->primaryKey => $id));
+      $this->request->data = $this->Contest->find('first', $options);
+    }
+  }
 
 /**
  * admin_delete method
@@ -167,17 +167,17 @@ class ContestsController extends AppController {
  * @param string $id
  * @return void
  */
-	public function admin_delete($id = null) {
-		$this->Contest->id = $id;
-		if (!$this->Contest->exists()) {
-			throw new NotFoundException(__('Invalid contest'));
-		}
-		$this->request->onlyAllow('post', 'delete');
-		if ($this->Contest->delete()) {
-			$this->Session->setFlash(__('Contest deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Contest was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+  public function admin_delete($id = null) {
+    $this->Contest->id = $id;
+    if (!$this->Contest->exists()) {
+      throw new NotFoundException(__('Invalid contest'));
+    }
+    $this->request->onlyAllow('post', 'delete');
+    if ($this->Contest->delete()) {
+      $this->Session->setFlash(__('Contest deleted'));
+      $this->redirect(array('action' => 'index'));
+    }
+    $this->Session->setFlash(__('Contest was not deleted'));
+    $this->redirect(array('action' => 'index'));
+  }
 }
--- Model/Contest.php
+++ Model/Contest.php
@@ -11,33 +11,33 @@ class Contest extends AppModel {
  *
  * @var string
  */
-	public $displayField = 'maintext';
+  public $displayField = 'maintext';
 
 /**
  * Validation rules
  *
  * @var array
  */
-	public $validate = array(
-		'maintext' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'image' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+  public $validate = array(
+    'maintext' => array(
+      'notempty' => array(
+        'rule' => array('notempty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+    'image' => array(
+      'notempty' => array(
+        'rule' => array('notempty'),
+        //'message' => 'Your custom message here',
+        //'allowEmpty' => false,
+        //'required' => false,
+        //'last' => false, // Stop validation after this rule
+        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+      ),
+    ),
+  );
 }
