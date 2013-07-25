58d4a67a345f9f45e0474ce84b20c54ae58d7029
Sebastian de Castelberg <sebu@kpricorn.org>
2013-07-25 16:40:38 +0200

    re-generate schema

--- Config/Schema/schema.sql
+++ Config/Schema/schema.sql
@@ -1,5 +1,5 @@
 
-[36mWelcome to CakePHP v2.3.6 Console[0m
+[36mWelcome to CakePHP v2.3.8 Console[0m
 ---------------------------------------------------------------
 App : skiresort_app_cms
 Path: /Users/sdecaste/devel/winterlife/skiresort_app_cms/
@@ -9,12 +9,23 @@ Cake Schema Shell
 
 
 DROP TABLE IF EXISTS `resort_app_disentis`.`contests`;
+DROP TABLE IF EXISTS `resort_app_disentis`.`users`;
 
 
 CREATE TABLE `resort_app_disentis`.`contests` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
 	`maintext` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
 	`image` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+	`created` datetime DEFAULT NULL,
+	`modified` datetime DEFAULT NULL,	PRIMARY KEY  (`id`)) 	DEFAULT CHARSET=utf8,
+	COLLATE=utf8_general_ci,
+	ENGINE=InnoDB;
+
+CREATE TABLE `resort_app_disentis`.`users` (
+	`id` int(10) NOT NULL AUTO_INCREMENT,
+	`username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+	`password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+	`role` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
 	`created` datetime DEFAULT NULL,
 	`modified` datetime DEFAULT NULL,	PRIMARY KEY  (`id`)) 	DEFAULT CHARSET=utf8,
 	COLLATE=utf8_general_ci,
