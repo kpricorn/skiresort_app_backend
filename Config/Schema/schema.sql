18add26f2d2e39d4d137166c290fa61ddbef6a41
Sebastian de Castelberg <sebu@kpricorn.org>
2013-11-24 18:02:58 +0100

    make schema.sql script database name agnostic

--- Config/Schema/schema.sql
+++ Config/Schema/schema.sql
@@ -8,11 +8,11 @@ Cake Schema Shell
 ---------------------------------------------------------------
 
 
-DROP TABLE IF EXISTS `resort_app_disentis`.`events`;
-DROP TABLE IF EXISTS `resort_app_disentis`.`users`;
+DROP TABLE IF EXISTS `events`;
+DROP TABLE IF EXISTS `users`;
 
 
-CREATE TABLE `resort_app_disentis`.`events` (
+CREATE TABLE `events` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
 	`maintext` text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
 	`image` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE `resort_app_disentis`.`events` (
 	COLLATE=utf8_general_ci,
 	ENGINE=InnoDB;
 
-CREATE TABLE `resort_app_disentis`.`users` (
+CREATE TABLE `users` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
 	`username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
 	`password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
